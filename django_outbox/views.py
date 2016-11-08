06231afc397a20327e0afef71c4a68010cbbff09
Alessandro Hecht <alessandrohc@gmail.com>
2016-11-08 19:07:08 -0200

    Update views.py
    Resolving Attachments

--- django_outbox/views.py
+++ django_outbox/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
-
+from django.http import HttpResponse
+import base64
 from .outbox import Outbox
 
 
@@ -15,10 +16,17 @@ class OutboxTemplateView(TemplateView):
 class MailTemplateView(TemplateView):
     template_name = 'django_outbox/mail.html'
 
+    def render_to_response(self, context, **kwargs):
+        contenttype = context['content_type']
+        if contenttype == "text/html" or contenttype == "text/plain":
+            return super(MailTemplateView, self).render_to_response(context, **kwargs)
+        return HttpResponse(base64.b64decode(context['mail'].body[contenttype]), content_type=contenttype)
+
     def get_context_data(self, id, **kwargs):
         context = super(MailTemplateView, self).get_context_data(**kwargs)
         mail = Outbox().get(id)
         context['mail'] = mail
         context['content_type'] = self.request.GET['content_type']
         context['content'] = mail.body[self.request.GET['content_type']]
+
         return context
