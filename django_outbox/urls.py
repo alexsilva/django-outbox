1ed501e6a8d70b374ecbe17ab23d4533bd3eb405
alexsilva <geniofuturo@gmail.com>
2024-04-29 10:18:03 -0300

    Adaptations for django4.

--- django_outbox/urls.py
+++ django_outbox/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import url
-
+try:
+    from django.urls import re_path as url
+except ImportError:
+    from django.conf.urls import url
 from django_outbox.views import OutboxTemplateView, MailTemplateView
 
 urlpatterns = [
