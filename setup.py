ec8fa5ddee49ec9b6ad0a20b3269858eb5f7fbf4
alexsilva <geniofuturo@gmail.com>
2024-09-20 16:46:01 -0300

    v0.9.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-outbox',
-    version='0.8.0',
+    version='0.9.0',
     packages=['django_outbox'],
     include_package_data=True,
     license='MIT License',
